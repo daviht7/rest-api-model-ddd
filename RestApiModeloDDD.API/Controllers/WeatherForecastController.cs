936a7b0530f08abf1a2a84096cded1087e7dac91
davi holanda <daviht7@gmail.com>
2023-09-09 20:32:40 -0300

    teste

--- RestApiModeloDDD.API/Controllers/WeatherForecastController.cs
+++ RestApiModeloDDD.API/Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ namespace RestApiModeloDDD.API.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching", "TESTE"
     };
 
         private readonly ILogger<WeatherForecastController> _logger;
